143178dca00754900882d0567eae298678f7bc23
Kevin <kandreyrosales@gmail.com>
2024-01-12 10:27:44 -0500

    Campos iniciales

--- site/ENCUESTAS/models.py
+++ site/ENCUESTAS/models.py
@@ -1,31 +1,137 @@
-# Create your models here.
-import datetime
-
 from django.db import models
-from django.utils import timezone
-from django.contrib import admin
+from multiselectfield import MultiSelectField
+
+TIPO_POBLACION = (
+    ("INDIGENA", 'Indígena'),
+    ("AFROCOLOMBIANA", 'Afrocolombiano (a)'),
+    ("CAMPESINA", 'Campesino (a)'),
+    ("ROM", 'Rom (Gitanos)'),
+    ("RAIZAL", 'Raizal'),
+    ("OTRO", 'Otro (a)'),
+    ("NINGUNO", 'Ninguno (a)')
+)
+
+ZONAS_ENTIDADES = (
+    ("CABECERA", 'Cabecera (Zona urbana)'),
+    ("CENTRO POBALDO", 'Centro poblado, rural disperso (Zona rural)'),
+    ("OTRA", 'Otra'),
+)
+
+GRUPOS_PERSONAS = [
+        ('Desplazado(a)', 'Desplazado(a)'),
+        ('Reincorporado(a)', 'Reincorporado(a)'),
+        ('Exiliado(a)', 'Exiliado(a)'),
+        ('Migrante(a)', 'Migrante(a)'),
+        ('Retornado(a)', 'Retornado(a)'),
+        ('Refugiado(a)', 'Refugiado(a)'),
+        ('Asilado(a)', 'Asilado(a)'),
+        ('Otro(a)', 'Otro(a)'),
+        ('Ninguno(a)', 'Ninguno(a)'),
+    ]
+
+OPCIONES_CONDICION_DISCAPACIDAD = [
+        ('Física o motora', 'Física o motora'),
+        ('Sensorial (Sentidos)', 'Sensorial (Sentidos)'),
+        ('Intelectual', 'Intelectual'),
+        ('Psíquica', 'Psíquica'),
+        ('Visual', 'Visual'),
+        ('Múltiple', 'Múltiple'),
+        ('Otra', 'Otra'),
+        ('Ninguna', 'Ninguna'),
+    ]
+
+OPCIONES_SI_NO = [
+    ('Si', 'Si'),
+    ('No', 'No'),
+]
+
+OPCIONES_EDAD = [
+        ('0 a 5 años', '0 a 5 años'),
+        ('6 a 11 años', '6 a 11 años'),
+        ('12 a 17 años', '12 a 17 años'),
+        ('18 a 23 años', '18 a 23 años'),
+        ('24 a 29 años', '24 a 29 años'),
+        ('30 a 35 años', '30 a 35 años'),
+        ('36 a 40 años', '36 a 40 años'),
+        ('41 a 46 años', '41 a 46 años'),
+        ('47 a 52 años', '47 a 52 años'),
+        ('53 a 59 años', '53 a 59 años'),
+        ('60 a 65 años', '60 a 65 años'),
+        ('66 a 71 años', '66 a 71 años'),
+        ('72 a 77 años', '72 a 77 años'),
+        ('77 a 82 años', '77 a 82 años'),
+        ('83 a 88 años', '83 a 88 años'),
+        ('89 a 94 años', '89 a 94 años'),
+        ('95 y más', '95 y más'),
+    ]
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
-    def __str__(self):
-        return self.question_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    @admin.display(
-        boolean=True,
-        ordering="pub_date",
-        description="Published recently?",
-    )
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+class Encuesta(models.Model):
+    nombre_entidad_religiosa = models.CharField(
+        'Nombre de la Entidad Religiosa', blank=False, null=False, max_length=255, default="")
+    nombre_persona = models.CharField(
+        'Nombre de la persona que responde la encuesta', blank=False, null=False, max_length=255, default="")
+    tipo_afiliacion = models.CharField(
+        'Tipo de afiliación de la persona que responde la encuesta con la entidad religiosa',
+        blank=False, null=False, max_length=255, default="")
+    cedula = models.CharField('Cédula de la persona que responde', blank=False, null=False, max_length=50)
+    celular = models.CharField('Celular', blank=False, null=False, max_length=50)
+    correo = models.EmailField('Correo electrónico', blank=False, null=False)
+    celular_extra = models.CharField(
+        'Si tiene otro número de celular o número fijo de contacto escríbalo aquí', blank=True, null=True)  # modificado
+    zona_entidad_religiosa = models.CharField(
+        '¿En que zona se encuentra ubicada la entidad religiosa que representa?',
+        blank=False, null=False, max_length=255, default="", choices=ZONAS_ENTIDADES)
+    otra_zona_entidad = models.CharField(
+        'Otro tipo de zona seleccionada', blank=True, null=True, max_length=255)
+    anos_activa_entidad = models.PositiveIntegerField(
+        '¿Cuántos años lleva activa la entidad religiosa a la que representa?', blank=False, null=False)
+
+    #####
+    poblacion_entidad = MultiSelectField(
+        'De acuerdo con su cultura, pueblo o rasgos físicos, ¿Qué población atienden y ayuda su entidad religiosa? '
+        '(puede marcar varias)', blank=False, null=False, max_length=255, default="", choices=TIPO_POBLACION)
+    otra_poblacion_entidad = models.CharField(
+        'Otro tipo de población seleccionado', blank=True, null=True, max_length=255)
+
+    ####
+    grupos_personas = MultiSelectField(
+        'Dentro del siguiente grupo de personas, ¿Cuál atiende y beneficia su entidad religiosa? '
+        '(puede marcar varias)', blank=False, null=False, max_length=255, default="", choices=GRUPOS_PERSONAS)
+    otros_grupos_personas = models.CharField(
+        'Otro grupo de personas seleccionado', blank=True, null=True, max_length=255)
+
+    ####
+    atiende_poblacion_discapacitada = models.CharField(
+        '¿Atiende población en la entidad religiosa en condición de discapacidad?',
+        blank=False, null=False, max_length=255, default=OPCIONES_SI_NO[0][0], choices=OPCIONES_SI_NO)
+
+    tipos_discapacidades = MultiSelectField(
+        'Si respondió si en la pregunta anterior ¿Qué tipo de discapacidad '
+        'tiene la población que atiende su entidad religiosa?',
+        blank=True, null=True, max_length=255, default="", choices=OPCIONES_CONDICION_DISCAPACIDAD)
+
+    otra_discapacidad = models.CharField(
+        'Otro discapacidad seleccionada', blank=True, null=True, max_length=255)
+
+    #####
+    grupo_demografico = models.CharField(
+        'De acuerdo a la edad, ¿Cuál es el grupo demográfico principal al que sirve su Entidad religiosa? '
+        '(puede seleccionar varios)',
+        blank=True, null=True, max_length=255, default="", choices=OPCIONES_EDAD)
+
+    ### Partimos la pregunta en 2 para poder ser analizada mejor y solo recibir campos numericos aqui
+    distribucion_hombres_entidad = models.PositiveIntegerField(
+        'De acuerdo al sexo (Hombre),¿Cuál es la distribución porcentual '
+        'de este en su Entidad religiosa? (ejemplo: 40% hombres)', blank=False, null=False, default=0)
+
+    distribucion_mujeres_entidad = models.PositiveIntegerField(
+        'De acuerdo al sexo (Mujeres),¿Cuál es la distribución porcentual '
+        'de este en su Entidad religiosa? (ejemplo: 60% mujeres)', blank=False, null=False, default=0)
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.choice_text
+
+
+
+
+
--- site/PNUD/settings.py
+++ site/PNUD/settings.py
@@ -76,16 +76,12 @@ WSGI_APPLICATION = "PNUD.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": 
-    #{
-    #   "ENGINE": "django.db.backends.sqlite3",
-    #    "NAME": BASE_DIR / "db.sqlite3",
-    #}, 
+    "default":
     {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "user",
-        "PASSWORD": "password123",
+        "NAME": "pnud_local",
+        "USER": "postgres",
+        "PASSWORD": "protools",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
